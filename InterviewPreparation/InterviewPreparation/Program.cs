2f94440ae79cb638a9bde95f2bfa32a4e1da2b85
vijay_cs <vijay_cs@outlook.com>
2021-05-09 12:28:37 +0530

    Understand the Throw and Throw ex

--- InterviewPreparation/InterviewPreparation/Program.cs
+++ InterviewPreparation/InterviewPreparation/Program.cs
@@ -10,17 +10,69 @@ namespace InterviewPreparation
     {
         static void Main(string[] args)
         {
-            int refValue=0;
-            int outValue;
+            try
+            {
+                FunThrowEx();
+            }
+            catch (Exception ex)
+            {
+                // When use throw ex then is will reset the stack trace of error.
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("\n end throw ex \n");
+                Console.WriteLine("\n");
+            }
 
-            OutVsRef.RefFoo(ref refValue);
 
-            Console.WriteLine(refValue);
+            try
+            {
+                FunThrow();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n start throw \n");
+                // When use throw then is will reset the stack trace of error.
+                Console.WriteLine(ex.StackTrace);
+            }
 
-            OutVsRef.OutFoo(out outValue);
+            Console.ReadLine();
+        }
 
-            Console.WriteLine(outValue);
 
+        /// <summary>
+        /// Fun Throw Ex
+        /// </summary>
+        static void FunThrowEx()
+        {
+            try
+            {
+                Fun2();
+            }
+            catch (Exception ex)
+            {
+                //When use default throw ex then it will reset the stack trace of error.
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Fun Throw
+        /// </summary>
+        static void FunThrow()
+        {
+            try
+            {
+                Fun2();
+            }
+            catch (Exception)
+            {
+                //When use default throw then is will help to show the actual stack trace of error.
+                throw;
+            }
+        }
+
+        static void Fun2()
+        {
+            throw new Exception("Exception from Fun2");
         }
     }
 }
