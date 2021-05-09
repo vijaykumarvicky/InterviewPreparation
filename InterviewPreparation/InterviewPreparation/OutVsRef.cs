d02a20e525730e96204b86b4dd0e065216c411be
vijay_cs <vijay_cs@outlook.com>
2021-05-09 11:52:10 +0530

    Check Out and Ref type variable uses.

---
+++ InterviewPreparation/InterviewPreparation/OutVsRef.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation
+{
+    public class OutVsRef
+    {
+        public static void RefFoo(ref int inputValue)
+        {
+            inputValue = inputValue + 20;
+        }
+
+        /// <summary>
+        /// This method is used for check out type variable
+        /// Out is only one way
+        /// Intialize value before using out variable
+        /// </summary>
+        /// <param name="inputValue"></param>
+        public static void OutFoo(out int inputValue)
+        {
+            //must to be initialize before use out type of variable.
+            inputValue = 5;
+
+            inputValue = inputValue + 20;
+        }
+
+    }
+}
---
+++ InterviewPreparation/InterviewPreparation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int refValue=0;
+            int outValue;
+
+            OutVsRef.RefFoo(ref refValue);
+
+            Console.WriteLine(refValue);
+
+            OutVsRef.OutFoo(out outValue);
+
+            Console.WriteLine(outValue);
+
+        }
+    }
+}
